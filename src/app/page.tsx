e06c859a48255b9af05a0cd9aa945fc3ec363a2d
kurealnum <oscar.gaske.cs@gmail.com>
2025-06-09 16:30:33 -0400

    feat: Added landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="">Fredericksburg Composite Mountain Bike Team</h1>
-    </div>
+    <>
+      <div className="absolute right-0 left-[50%] z-10 w-screen -translate-x-[50%] translate-y-[37vh] overflow-hidden [@media(max-height:700px)]:translate-y-[30vh]">
+        <h1 className="w-screen text-center text-4xl text-shadow-lg/25 md:text-5xl lg:text-6xl">
+          Fredericksburg Composite Mountain Bike Team
+        </h1>
+        <Button
+          className="relative left-[50%] mt-6 -translate-x-[50%]"
+          variant={"secondary"}
+        >
+          About us
+        </Button>
+      </div>
+      <div className="relative h-screen overflow-hidden">
+        <Image
+          src={"/landingpic.jpg"}
+          width={1503}
+          height={762}
+          alt=""
+          className="ml-[50%] h-screen w-[200vh] max-w-[200vh] -translate-x-[50%] scale-105 overflow-clip opacity-85 blur-md 2xl:scale-150"
+        />
+      </div>
+    </>
   );
 }
