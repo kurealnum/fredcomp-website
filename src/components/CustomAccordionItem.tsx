1039aa259c0c59a27205b0b9ec103dca48d819eb
kurealnum <oscar.gaske.cs@gmail.com>
2025-06-12 16:43:16 -0400

    feat: Added FAQ

---
+++ src/components/CustomAccordionItem.tsx
@@ -0,0 +1,32 @@
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./ui/accordion";
+
+export default function CustomAccordionItem({
+  trigger,
+  className,
+  id,
+  triggerClassName,
+  contentClassName,
+  children,
+}: {
+  trigger: string;
+  id: string;
+  className?: string;
+  triggerClassName?: string;
+  contentClassName?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <AccordionItem value={id} className={className + " font-light"}>
+      <AccordionTrigger className={triggerClassName + " text-md font-normal"}>
+        {trigger}
+      </AccordionTrigger>
+      <AccordionContent className={contentClassName}>
+        {children}
+      </AccordionContent>
+    </AccordionItem>
+  );
+}
