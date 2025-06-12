a60c3814ea56a8ba1bb7cf45e6543680288cd196
kurealnum <oscar.gaske.cs@gmail.com>
2025-06-12 10:54:01 -0400

    feat: Added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body className={`${outfit.className} text-black antialiased`}>
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,105 @@
+import { Separator } from "./ui/separator";
+import Monolith from "@img/monolith.png";
+import CMV from "@img/custommetals.png";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="bg-primary mt-8 font-extralight text-white [&_h1]:text-xl">
+      <div className="max-w[1000px] flex flex-row flex-wrap justify-center gap-10 py-4">
+        <div>
+          <h1 className="mx-auto w-min font-normal text-nowrap">
+            Helpful Links
+          </h1>
+          <ul className="flex flex-col *:hover:underline">
+            <li>
+              <a href="https://github.com/kurealnum/fredcomp-website/issues">
+                Make a feature request
+              </a>
+            </li>
+            <li>
+              <Link href={"/volunteer"}>Become a coach</Link>
+            </li>
+            <li>
+              <Link href={"/join"}>Register for the team</Link>
+            </li>
+            <li>
+              <Link href={"/join"}>Pay team fees</Link>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/FredCompMTB/">
+                FredComp Official Facebook
+              </a>
+            </li>
+            <li>
+              <a href="https://nationalmtb.org/">NICA</a>
+            </li>
+            <li>
+              <a href="https://vahs-sports.com/">VAHS</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="mx-auto w-min font-normal text-nowrap">Sponsors</h1>
+          <ul className="flex flex-col items-center justify-center">
+            <li>
+              <Image
+                className="w-[300px]"
+                src={Monolith}
+                alt="Monolith Performance provides high quality nutrition for endurance athletes seeking optimized science-backed products that provide fuel for the extra mile at a price point that allows you to focus on your ride, not your wallet."
+              />
+            </li>
+            <li>
+              <Image
+                className="w-[250px]"
+                src={CMV}
+                alt="Custom Metals of Virginia provides exceptional custom metal fabrication to homeowners, architects, and businesses."
+              />
+            </li>
+            <li className="mt-2">
+              <Link href={"/donate"} className="text-secondary hover:underline">
+                Interested in being a sponsorer?
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="mx-auto w-min font-normal text-nowrap">Contact us</h1>
+          <div className="text-center *:hover:underline">
+            <p>
+              <a href="mailto:fredcompmtb@gmail.com">fredcompmtb@gmail.com</a>
+            </p>
+            <p>
+              <a href="tel:+15402208473">540-220-8473</a>
+            </p>
+          </div>
+          <div className="mx-auto mt-4 w-[90%] max-w-[240px] text-xs">
+            <p className="mb-2">
+              Fredericksburg Composite MTB Team is supported by the nonprofit
+              501c3 organization Fredericksburg Area Interscholastic Cycling
+              Team (EIN 87-2871561).
+            </p>
+            <p>
+              (Legal jargin that states we are legally classified as a
+              non-profit, not a club or Nica team)
+            </p>
+          </div>
+        </div>
+      </div>
+      <Separator className="bg-accent" />
+      <div className="flex flex-row flex-wrap justify-center gap-4 py-4">
+        <p>
+          Website designed & developed by{" "}
+          <a
+            className="text-secondary hover:underline"
+            href="https://oscargaske.me"
+          >
+            Oscar Gaske
+          </a>
+        </p>
+        <p>Majority of photos taken by Emily Lumpkins</p>
+      </div>
+    </div>
+  );
+}
